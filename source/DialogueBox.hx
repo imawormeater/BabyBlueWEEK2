5f47f98c9ca4c3d399e5f617657c8138f1928243
imawormeater <buttfacecus@gmail.com>
2021-10-07 18:21:16 -0500

    dialouge shit added

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -58,24 +58,14 @@ class DialogueBox extends FlxSpriteGroup
 					FlxG.sound.playMusic(Paths.music('babytheme'), 0.485);
 					FlxG.sound.music.fadeIn(1, 0, 0.8);
 				}
-			case 'babys-revenge':
-				if (PlayState.isStoryMode) {
-					FlxG.sound.playMusic(Paths.music('babytheme'), 0.485);
-					FlxG.sound.music.fadeIn(1, 0, 0.8);
-				}
-			case 'un-adieu':
-				if (PlayState.isStoryMode) {
-					FlxG.sound.playMusic(Paths.music('babytheme'), 0.485);
-					FlxG.sound.music.fadeIn(1, 0, 0.8);
-				}
-			case 'temper-tantrum':
-				if (PlayState.isStoryMode) {
-					FlxG.sound.playMusic(Paths.music('babytheme'), 0.485);
-					FlxG.sound.music.fadeIn(1, 0, 0.8);
-				}
 			case 'trackstar':
 				if (PlayState.isStoryMode) {
-					FlxG.sound.playMusic(Paths.music('babytheme'), 0.485);
+					FlxG.sound.playMusic(Paths.music('bloop'), 0.485);
+					FlxG.sound.music.fadeIn(1, 0, 0.8);
+				}
+			case defualt:
+				if (PlayState.isStoryMode) {
+					FlxG.sound.playMusic(Paths.music('boomer'), 0.485);
 					FlxG.sound.music.fadeIn(1, 0, 0.8);
 				}
 		}
@@ -99,32 +89,32 @@ class DialogueBox extends FlxSpriteGroup
 		{
 			case 'kidz-bop':
 				hasDialog = true;
-				box.frames = Paths.getSparrowAtlas('speech_bubble_talking');
+				box.frames = Paths.getSparrowAtlas('speech_bubble_talking_baby');
 				box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
 				box.animation.addByPrefix('normal', 'Speech Bubble Normal Open', 24, false);
 				box.setGraphicSize(Std.int(box.width * 1 * 0.9));
-				box.y = (FlxG.height - box.height) + 80;
+				box.y = (FlxG.height - box.height) - 20;
 			case 'nap-time':
 				hasDialog = true;
-				box.frames = Paths.getSparrowAtlas('speech_bubble_talking');
+				box.frames = Paths.getSparrowAtlas('speech_bubble_talking_baby');
 				box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
 				box.animation.addByPrefix('normal', 'Speech Bubble Normal Open', 24, false);
 				box.setGraphicSize(Std.int(box.width * 1 * 0.9));
-				box.y = (FlxG.height - box.height) + 80;
+				box.y = (FlxG.height - box.height) - 20;
 			case 'baby-blue':
 				hasDialog = true;
-				box.frames = Paths.getSparrowAtlas('speech_bubble_talking');
+				box.frames = Paths.getSparrowAtlas('speech_bubble_talking_baby');
 				box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
 				box.animation.addByPrefix('normal', 'Speech Bubble Normal Open', 24, false);
 				box.setGraphicSize(Std.int(box.width * 1 * 0.9));
-				box.y = (FlxG.height - box.height) + 80;
+				box.y = (FlxG.height - box.height) - 20;
 			case defualt:
 				hasDialog = true;
-				box.frames = Paths.getSparrowAtlas('speech_bubble_talking');
+				box.frames = Paths.getSparrowAtlas('speech_bubble_talking_baby');
 				box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
 				box.animation.addByPrefix('normal', 'Speech Bubble Normal Open', 24, false);
 				box.setGraphicSize(Std.int(box.width * 1 * 0.9));
-				box.y = (FlxG.height - box.height) + 80;
+				box.y = (FlxG.height - box.height) - 20;
 		}
 
 		this.dialogueList = dialogueList;
@@ -196,7 +186,6 @@ class DialogueBox extends FlxSpriteGroup
 		{
 			if (box.animation.curAnim.name == 'normalOpen' && box.animation.curAnim.finished)
 			{
-				box.animation.play('normal');
 				dialogueOpened = true;
 			}
 		}
@@ -314,6 +303,66 @@ class DialogueBox extends FlxSpriteGroup
 					portraitLeft.x = box.x + 64;
 					portraitLeft.y = box.y - 196;
 
+					portraitLeft.visible = true;
+					portraitLeft.animation.play('enter');
+				}
+			case 'evil-baby':
+				swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 1)];
+				swagDialogue.color = FlxColor.fromRGB(26, 96, 237);
+				portraitRight.visible = false;
+				if (!portraitLeft.visible)
+				{
+					portraitLeft.frames = Paths.getSparrowAtlas('dialogue/ports');
+					portraitLeft.animation.addByPrefix('enter', 'evil baby port', 24, false);
+					portraitLeft.scale.set(1.3, 1.3);
+					portraitLeft.antialiasing = true;
+					portraitLeft.updateHitbox();
+					portraitLeft.scrollFactor.set();
+					// portraitLeft.screenCenter(X);
+	
+					portraitLeft.x = box.x + 64;
+					portraitLeft.y = box.y - 196;
+	
+					portraitLeft.visible = true;
+					portraitLeft.animation.play('enter');
+				}
+			case 'goblin':
+				swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 1)];
+				swagDialogue.color = FlxColor.fromRGB(26, 96, 237);
+				portraitRight.visible = false;
+				if (!portraitLeft.visible)
+				{
+					portraitLeft.frames = Paths.getSparrowAtlas('dialogue/ports');
+					portraitLeft.animation.addByPrefix('enter', 'goblin port', 24, false);
+					portraitLeft.scale.set(1.3, 1.3);
+					portraitLeft.antialiasing = true;
+					portraitLeft.updateHitbox();
+					portraitLeft.scrollFactor.set();
+					// portraitLeft.screenCenter(X);
+	
+					portraitLeft.x = box.x + 50;
+					portraitLeft.y = box.y - 196;
+	
+					portraitLeft.visible = true;
+					portraitLeft.animation.play('enter');
+				}
+			case 'lol':
+				swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 1)];
+				swagDialogue.color = FlxColor.fromRGB(26, 96, 237);
+				portraitRight.visible = false;
+				if (!portraitLeft.visible)
+				{
+					portraitLeft.frames = Paths.getSparrowAtlas('dialogue/ports');
+					portraitLeft.animation.addByPrefix('enter', 'lol port', 24, false);
+					portraitLeft.scale.set(1.3, 1.3);
+					portraitLeft.antialiasing = true;
+					portraitLeft.updateHitbox();
+					portraitLeft.scrollFactor.set();
+					// portraitLeft.screenCenter(X);
+	
+					portraitLeft.x = box.x + 64;
+					portraitLeft.y = box.y - 196;
+	
 					portraitLeft.visible = true;
 					portraitLeft.animation.play('enter');
 				}
