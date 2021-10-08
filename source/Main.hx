6140f89d547dcd0a5b5260061d6ad3fcac05c28d
imawormeater <buttfacecus@gmail.com>
2021-10-08 14:30:37 -0500

    OOps

--- source/Main.hx
+++ source/Main.hx
@@ -74,7 +74,7 @@ class Main extends Sprite
 		}
 
 		#if !debug
-		initialState = EndState;
+		initialState = TitleState;
 		#end
 
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
